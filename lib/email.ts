d421db1c861a457d593dfc19ddc26c0f1dbcc15b
Daan Lenaerts <me@daanlenaerts.com>
2025-09-17 18:28:51 +0200

    Extended email capabilities

--- lib/email.ts
+++ lib/email.ts
@@ -1,29 +1,32 @@
 import { render } from "@react-email/render";
 import type { ReactElement } from "react";
-import { ServerClient } from "postmark";
+import { Attachment, ServerClient } from "postmark";
 import "dotenv/config";
 
 interface EmailOptions {
+  from?: string;
   to: string;
   subject: string;
-  email: ReactElement;
+  email: ReactElement | string;
   replyTo?: string;
+  attachments?: Attachment[];
 }
 
-export async function sendEmail({ to, subject, email, replyTo }: EmailOptions) {
+export async function sendEmail({ to, subject, email, replyTo, from, attachments }: EmailOptions) {
   if (!process.env.POSTMARK_API_KEY) {
     throw new Error("POSTMARK_API_KEY is not set");
   }
 
   const postmarkClient = new ServerClient(process.env.POSTMARK_API_KEY);
-  const emailHtml = await render(email);
+  const emailHtml = typeof email === "string" ? email : await render(email);
 
   const options = {
-    From: process.env.EMAIL_FROM || "support@recommand.eu",
+    From: from || process.env.EMAIL_FROM || "support@recommand.eu",
     To: to,
     Subject: subject,
     HtmlBody: emailHtml,
     ReplyTo: replyTo,
+    Attachments: attachments,
   };
 
   try {
