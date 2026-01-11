eb96b3a675349e35e4bc60a7edc6ec66498c3081
kobeindemans <kobe.indemans@gmail.com>
2026-01-11 11:30:05 +0100

    Updated destructive button styles

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -12,13 +12,15 @@ const buttonVariants = cva(
         default:
           "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
         destructive:
-          "bg-destructive text-destructive-foreground shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+          "border border-destructive text-destructive bg-background shadow-xs hover:bg-destructive hover:text-destructive-foreground focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40",
         outline:
           "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary:
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
+        "ghost-destructive":
+          "text-destructive hover:bg-destructive hover:text-destructive-foreground dark:hover:bg-destructive",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
